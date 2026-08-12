d6b5940a9c85e09cb7093fdbcd00f9e8c46d5792
Soyal Khan <soyalkhan53133@gmail.com>
2026-08-12 18:34:20 +0530

    today changes

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,12 +1,13 @@
 import Soyal from './Soyal';
 
 function App() {
+  const name = 'Soyal Khan Salesforce Developer';
   return (
     <> {/* fragment tag  <> </>    */}
       
       <Soyal /> 
       <h1>Welcome to React</h1>
-      <p>This is a simple React app.</p>
+      <p>This is a simple React app {name}</p>
     </>
   )
 }
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,9 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+function MyApp(){
+  return (
+    <div>   
+      <h1>Welcome to React</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children: 'click me to go to google'
+// }
+
+const anotherReactElement = (
+  <a href="https://www.google.com" target='_blank'> visit google </a>
+)
+    
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',
+    target: '_blank'
+  },
+  'click me to go to google'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+    //MyApp()
+    //  <MyApp />
     <App />
-  </StrictMode>,
+    //anotherReactElement
+    //reactElement
 )
