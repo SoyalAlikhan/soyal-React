2946f99d0822b07b46aa399049688a5a5fb3772a
Soyal Khan <soyalkhan53133@gmail.com>
2026-08-12 18:40:26 +0530

    c

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -6,9 +6,9 @@ function App() {
     <> {/* fragment tag  <> </>    */}
       
       <Soyal /> 
-      <h1>Welcome to React</h1>
+      <h1>Welcome to 1 React</h1>
       <p>This is a simple React app {name}</p>
-    </>
+    </> 
   )
 }
 
